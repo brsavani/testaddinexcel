fad32abf9b344eebc1c455383294e1c374964fdd
brsavani <bsavani@outlook.com>
2017-11-30 12:51:05 -0200

    Update Startup.Auth.cs

--- Office-Add-in-ASPNET-SSO-WebAPI/App_Start/Startup.Auth.cs
+++ Office-Add-in-ASPNET-SSO-WebAPI/App_Start/Startup.Auth.cs
@@ -4,40 +4,74 @@
     This file configures auth for the add-in. 
 */
 
-using Owin;
-using System.IdentityModel.Tokens;
+using Microsoft.Owin.Security.Cookies;
+using Microsoft.Owin;
 using System.Configuration;
-using Microsoft.Owin.Security.OAuth;
-using Microsoft.Owin.Security.Jwt;
-using Office_Add_in_ASPNET_SSO_WebAPI.App_Start;
+using Microsoft.Owin.Security.OpenIdConnect;
+using System.Threading.Tasks;
+using System.Globalization;
+using Microsoft.Owin.Security;
+using Owin;
 
 namespace Office_Add_in_ASPNET_SSO_WebAPI
 {
     public partial class Startup
     {
+        private static string clientId = ConfigurationManager.AppSettings["ida:ClientId"];
+        private static string appKey = ConfigurationManager.AppSettings["ida:AppKey"];
+        private static string aadInstance = ConfigurationManager.AppSettings["ida:AADInstance"];
+        private static string tenant = ConfigurationManager.AppSettings["ida:Tenant"];
+        private static string audience = ConfigurationManager.AppSettings["ida:Audience"];
+        private static string postLogoutRedirectUri = ConfigurationManager.AppSettings["ida:PostLogoutRedirectUri"];
+        private static string authority = string.Format(CultureInfo.InvariantCulture, aadInstance, tenant);
+        
         public void ConfigureAuth(IAppBuilder app)
         {
-            var tvps = new TokenValidationParameters
+                       
+            
+            app.SetDefaultSignInAsAuthenticationType(CookieAuthenticationDefaults.AuthenticationType);
+            app.UseCookieAuthentication(new CookieAuthenticationOptions());
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
             {
-                // Set the strings to validate against. (Scopes, which should be 
-                // simply "access_as_user" in this sample, is validated inside the controller.)
-                ValidAudience = ConfigurationManager.AppSettings["ida:Audience"],
-                ValidIssuer = ConfigurationManager.AppSettings["ida:Issuer"],
+                ClientId = clientId,
+                Authority = authority,
+                PostLogoutRedirectUri = postLogoutRedirectUri,
+                Notifications = new OpenIdConnectAuthenticationNotifications
+                {
+                    AuthenticationFailed = context =>
+                    {
+                        context.HandleResponse();
+                        context.Response.Redirect("Home/Error");
+                        return Task.FromResult(0);
+                    }
+                }
+            });
+            
+            
+            
+            
+            
+            //var tvps = new TokenValidationParameters
+            //{
+            //    // Set the strings to validate against. (Scopes, which should be 
+            //   // simply "access_as_user" in this sample, is validated inside the controller.)
+            //    ValidAudience = ConfigurationManager.AppSettings["ida:Audience"],
+            //    ValidIssuer = ConfigurationManager.AppSettings["ida:Issuer"],
 
                 // Save the raw token recieved from the Office host, so it can be 
                 // used in the "on behalf of" flow.
-                SaveSigninToken = true
+            //    SaveSigninToken = true
             };
 
             // The more familiar UseWindowsAzureActiveDirectoryBearerAuthentication does not work
             // with the Azure AD V2 endpoint, so use UseOAuthBearerAuthentication instead.
-            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions
-            {
-                AccessTokenFormat = new JwtFormat(tvps,
-
+           // app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions
+            //{
+          //      AccessTokenFormat = new JwtFormat(tvps,
+//
                     // Specify the discovery endpoint, also called the "metadata address".
-                    new OpenIdConnectCachingSecurityTokenProvider("https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration"))
-            });
+           //         new OpenIdConnectCachingSecurityTokenProvider("https://login.microsoftonline.com/common/v2.0/.well-known/openid-configuration"))
+           // });
         }
     }
 }
